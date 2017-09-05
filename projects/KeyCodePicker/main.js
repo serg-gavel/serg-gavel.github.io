b396181423fe02484bbe4cb4f8de49927f67e287
Serg <serg-gavel@users.noreply.github.com>
2017-09-06 01:24:48 +0300

    Update repository 06/09/2017

--- projects/KeyCodePicker/main.js
+++ projects/KeyCodePicker/main.js
@@ -1,1 +1,10 @@
-"use strict";document.addEventListener("keydown",function(event){document.body.innerHTML="n&nbsp;&nbsp;&nbsp;&nbsp;n"+"<b>Which: "+event.which+"</b>n"+"<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;n"+"<b> Key code is:</b> "+event.keyCode+"n"+"<br>&nbsp;&nbsp;&nbsp;n<b>Shift key active:</b> "+event.shiftKey+"n"+"</br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;n<b> Alt key active:</b> "+event.altKey+"n"+"<br>&nbsp;&nbsp;&nbsp;&nbsp;n<b> Ctrl key active:</b> "+event.ctrlKey+"n"+"<br>&nbsp;&nbsp;&nbsp;n<b> Meta key active:</b> "+event.metaKey+"n";});
+"use strict";
+document.addEventListener("keydown", function (event) {
+    document.body.innerHTML = "\n&nbsp;&nbsp;&nbsp;&nbsp;\n" +
+        "<b>Which: " + event.which + "</b>\n" +
+        "<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\n" + "<b> Key code is:</b> " + event.keyCode + "\n" +
+        "<br>&nbsp;&nbsp;&nbsp;\n<b>Shift key active:</b> " + event.shiftKey + "\n" +
+        "</br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\n<b> Alt key active:</b> " + event.altKey + "\n" +
+        "<br>&nbsp;&nbsp;&nbsp;&nbsp;\n<b> Ctrl key active:</b> " + event.ctrlKey + "\n" +
+        "<br>&nbsp;&nbsp;&nbsp;\n<b> Meta key active:</b> " + event.metaKey + "\n";
+});
