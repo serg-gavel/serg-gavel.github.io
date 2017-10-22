7f2f881dada8220085891fb5f4dfaf9b0ecdfb11
Sergey Gavelyuk <serg-gavel@users.noreply.github.com>
2017-10-22 05:55:01 +0300

    Add files via upload

--- projects/test/netpeak/js/basic.js
+++ projects/test/netpeak/js/basic.js
@@ -1,12 +1,33 @@
 $(document).ready(function() {
     $('.hamburger').click(function () {
         $('.mobile-nav').toggleClass('hidden');
-    })
+    });
 
 
+    $('a[href*="#"]')
+        .not('[href="#"]')
+        .not('[href="#0"]')
+        .click(function(event) {
+            if (
+                location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+                &&
+                location.hostname == this.hostname
+            ) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    event.preventDefault();
+                    $('html, body').animate({
+                        scrollTop: target.offset().top
+                    }, 1200, function() {
 
+                    });
+                }
+            }
+        });
 
 });
+
 
 
 $(window).on('load', function () {
